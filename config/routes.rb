59ae23de421a6b4a1d84a1a1ce49eaadef2ff93d
ikoote1 <ikootepreim@gmail.com>
2023-08-17 18:34:56 +0300

    finalizing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'greetings_api/random'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'random_greeting', to: 'greetings_api#random'
+
 end
